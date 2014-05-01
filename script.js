80987d39c5cef7143cc179230e9431d1647e5e71
techhead55 <techhead55@gmail.com>
2014-05-01 19:13:03 +1000

    Added animations to game, Added jquery Library, Refactored some parts of code.

--- script.js
+++ script.js
@@ -16,30 +16,32 @@
             if (won){
                 
             } else {
-                var seq = this.value,
-                    last = seq[seq.length-1];
-                if (isNaN(parseFloat(last))){
-                    this.value = seq.substring(0, seq.length-1);
-                }
-                if (seq.length > 5){
-                    this.value = seq.substring(0, 5);
-                }
+                this.value = isNaN(parseFloat(this.value[this.value.length-1])) ? this.value.substring(0, this.value.length-1) : this.value;
             }
         };
         function output(){
             var element = document.createElement("div"),
                 rowCountString = rowcount+"",
                 correctPlace = 0,
-                correctNumber = 0;
-            outer:
+                correctNumber = 0,
+                blackList = [];
             for (var i=0; i<inp.value.length; i++){
                 if (inp.value[i]===num[i]){
                     correctPlace++;
                 }
+            }
+            for (var i=0; i<inp.value.length; i++){
+                labelled:
                 for (var x=0; x<num.length; x++){
                     if (inp.value[i]===num[x]){
+                        for (var y=0; y<blackList.length; y++){
+                            if (num[x]===blackList[y]){
+                                continue labelled;
+                            }
+                        }
                         correctNumber++;
-                        continue outer;
+                        blackList.push(x);
+                        break;
                     }
                 }
             }
@@ -47,30 +49,47 @@
                 rowCountString = "0"+rowCountString;
             }
             rowcount += 1;
+            $("input").animate({
+                marginTop: "-60px"
+            }, 300, function(){
+                out.appendChild(element);
+                inp.style.marginTop = (((window.innerHeight)/2)+20)
+                if (won){
+                    inp.style.color = "#A4C639";
+                    inp.value = "Correct";
+                    inp.setAttribute("readonly","");
+                    inp.onclick = function(){
+                        window.location.reload()
+                    };
+                } else {
+                    inp.value = "";
+                }
+                $("input").animate({
+                    marginTop: "-20px"
+                }, 600, function(){
+                    if (won){
+                        $("#container").animate({
+                            height: "0"
+                        }, 600);
+                    }
+                });
+            });
             element.setAttribute("class", "row");
             element.innerHTML = rowCountString+" "+inp.value+" "+correctPlace+correctNumber;
-            out.appendChild(element);
+            
         }
         inp.onkeydown = function(e){
             code = e.keyCode;
             if (code===13){
                 if (!(inp.value.length < 5)){
                     if (inp.value === num){
-                        var element = document.createElement("div"),
-                            rowCountString = rowcount+"",
-                            correctPlace = 0,
-                            correctNumber = 0;
                         output();
                         won = true;
-                        inp.style.color = "#A4C639";
-                        inp.value = "Correct";
-                        inp.setAttribute("readonly","");
                     } else {
                         if (won){
                             window.location.reload()
                         } else {
                             output();
-                            inp.value = "";
                         }
                     }
                 }
